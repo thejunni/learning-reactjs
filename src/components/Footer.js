5e507d33a153ff12b5b31e459fc76be199b84e23
thejunni <juniarta633@gmail.com>
2023-04-17 15:27:04 +0800

    menggunakan class component dan memanggil props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
     render(){
         return (
             <div>
                 <h1>Ini Footer</h1>
+                {this.props.letters()}
             </div>
         )
     }
